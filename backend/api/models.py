0bf764f024f684994740e157ae25a4319da31d23
TheL0nelyG0d <cetoerdem1@gmail.com>
2024-07-26 19:28:28 +0300

    revert:first launch

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+# Create your models here.
+
 
 
 class Note(models.Model):
@@ -7,6 +9,6 @@ class Note(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
-
-    def __str__(self):
+    
+    def __str__(self) -> str:
         return self.title
--- backend/api/views.py
+++ backend/api/views.py
@@ -6,28 +6,39 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Note
 
 
+# Create your views here.
+
+
+
 class NoteListCreate(generics.ListCreateAPIView):
     serializer_class = NoteSerializer
     permission_classes = [IsAuthenticated]
-
+    
+    
     def get_queryset(self):
         user = self.request.user
         return Note.objects.filter(author=user)
-
-    def perform_create(self, serializer):
-        if serializer.is_valid():
-            serializer.save(author=self.request.user)
+    
+    
+    def perform_create(self, seriazlier):
+        if seriazlier.is_valid():
+            seriazlier.save(author=self.request.user)
         else:
-            print(serializer.errors)
+            print(seriazlier.errors)
 
 
 class NoteDelete(generics.DestroyAPIView):
-    serializer_class = NoteSerializer
+    serializer_class =NoteSerializer
     permission_classes = [IsAuthenticated]
-
+    
+    
     def get_queryset(self):
         user = self.request.user
         return Note.objects.filter(author=user)
+
+
+
+
 
 
 class CreateUserView(generics.CreateAPIView):
